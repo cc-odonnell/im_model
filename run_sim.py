1805709c03dcf26550ab604a63ab8c4c436a2b9f
Cecilia Odonnell <cici@Cecilias-MacBook-Pro.local>
2024-09-13 09:44:08 -0700

    get summary stats from simulation

--- run_sim.py
+++ run_sim.py
@@ -1,9 +1,8 @@
 import numpy as np
 
-
 # get data from 'generate_data.py'
 # subset to 1 SKU-DC combo
-demand_data = df[(df['item_num']==10000) & (df['dc']=='A')]
+demand_data = df[(df['item_num'] == 10000) & (df['dc'] == 'A')]
 
 # make sure date is set as index
 demand_data = demand_data.reset_index(drop=True)
@@ -22,7 +21,6 @@ demand_data['delivery'] = 0
 
 # set the first day of inventory
 demand_data.loc[demand_data.index[0], 'on_hand_il'] = 11111
-
 
 '''
 # R-style version of function 
@@ -60,18 +58,20 @@ def proj_inv_in_transit(demand_data):
 df2 = proj_inv_in_transit(demand_data)
 '''
 
+
 # Pandas-style version of function with .at method
 def proj_inv_in_transitv2(demand_data):
     lead_time = 10
-    reorder_pt = 8000*5
-    reorder_qty = 8000*10
+    reorder_pt = 8000 * 5
+    reorder_qty = 8000 * 10
 
-# index starts at 0, so 1 is the second row
+    # index starts at 0, so 1 is the second row
     for i in range(1, len(demand_data)):
 
         # calculate on-hand inventory, min floor inventory level to 0
         demand_data.at[i, 'on_hand_il'] = max(0,
-                                   demand_data.at[i, 'delivery'] + demand_data.at[i -1, 'on_hand_il'] - demand_data.at[i, 'daily_demand'])
+                                              demand_data.at[i, 'delivery'] + demand_data.at[i - 1, 'on_hand_il'] -
+                                              demand_data.at[i, 'daily_demand'])
 
         # calculate total inventory (on hand and in transit)
         demand_data.at[i, 'total_il'] = demand_data.at[i, 'on_hand_il'] + demand_data.at[i, 'in_transit_il']
@@ -85,13 +85,19 @@ def proj_inv_in_transitv2(demand_data):
                 demand_data.at[i + lead_time, 'delivery'] = demand_data.at[i + lead_time, 'delivery'] + reorder_qty
 
                 # add in_transit inventory to each day (i+1) through (i + LT - 1)
-                demand_data.loc[i + 1: i + lead_time, 'in_transit_il'] = demand_data.loc[i + 1: i + lead_time, 'in_transit_il'] + reorder_qty
+                demand_data.loc[i + 1: i + lead_time, 'in_transit_il'] = demand_data.loc[i + 1: i + lead_time,
+                                                                         'in_transit_il'] + reorder_qty
 
     return demand_data
 
-df2 = proj_inv_in_transitv2(demand_data)
 
-# next version should incorporate a rq_data dataframe
+df2 = proj_inv_in_transitv2(demand_data)
+# new column
+demand_data['unmet_demand'] = (demand_data['new_daily_demand'] - demand_data['on_hand_il']).clip(lower=0)
+# save data
+demand_data.to_csv('demand_data_sim1.csv')
+
+# later version should incorporate a rq_data dataframe
 
 '''
   lead_time <- 10
