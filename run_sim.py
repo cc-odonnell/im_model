912b68aa3b444b9d8a0abaa154dcd9351217d0a9
Cecilia Odonnell <cici@Cecilias-MacBook-Pro.local>
2024-09-20 13:33:24 -0700

    convert sim function to have 2 loops

--- run_sim.py
+++ run_sim.py
@@ -35,43 +35,43 @@ starting_value = rq_inputs.loc[rq_inputs.index[0], 'reorder_pt'] + demand_data.l
 demand_data.loc[demand_data.index[0], 'on_hand_il'] = starting_value
 
 
-# Pandas-style version of function with .at method
 def sim_il(demand_data, rq_inputs):
-    lead_time = rq_inputs.loc[rq_inputs.index[0], 'lead_time']
-    reorder_pt = rq_inputs.loc[rq_inputs.index[0],'reorder_pt']
-    reorder_qty = rq_inputs.loc[rq_inputs.index[0],'reorder_qty']
+    for j in range(0, len(rq_inputs)):
+        lead_time = rq_inputs.loc[rq_inputs.index[j], 'lead_time']
+        reorder_pt = rq_inputs.loc[rq_inputs.index[j],'reorder_pt']
+        reorder_qty = rq_inputs.loc[rq_inputs.index[j],'reorder_qty']
 
     # index starts at 0, so 1 is the second row
-    for i in range(1, len(demand_data)):
+        for i in range(1, len(demand_data)):
 
-        # calculate on-hand inventory, min floor inventory level to 0
-        demand_data.at[i, 'on_hand_il'] = max(0,
-                                              demand_data.at[i, 'delivery'] + demand_data.at[i - 1, 'on_hand_il'] -
-                                              demand_data.at[i, 'daily_demand'])
+            # calculate on-hand inventory, min floor inventory level to 0
+            demand_data.at[i, 'on_hand_il'] = max(0,
+                                                  demand_data.at[i, 'delivery'] + demand_data.at[i - 1, 'on_hand_il'] -
+                                                  demand_data.at[i, 'daily_demand'])
 
-        # calculate total inventory (on hand and in transit)
-        demand_data.at[i, 'total_il'] = demand_data.at[i, 'on_hand_il'] + demand_data.at[i, 'in_transit_il']
+            # calculate total inventory (on hand and in transit)
+            demand_data.at[i, 'total_il'] = demand_data.at[i, 'on_hand_il'] + demand_data.at[i, 'in_transit_il']
 
-        # trigger order if total inventory <= reorder point
-        if demand_data.at[i, 'total_il'] <= reorder_pt:
+            # trigger order if total inventory <= reorder point
+            if demand_data.at[i, 'total_il'] <= reorder_pt:
 
-            # supply arrives lead time days later (if within simulation time period)
-            if (i + lead_time < len(demand_data)):
-                # add supply
-                demand_data.at[i + lead_time, 'delivery'] = demand_data.at[i + lead_time, 'delivery'] + reorder_qty
+                # supply arrives lead time days later (if within simulation time period)
+                if (i + lead_time < len(demand_data)):
+                    # add supply
+                    demand_data.at[i + lead_time, 'delivery'] = demand_data.at[i + lead_time, 'delivery'] + reorder_qty
 
-                # add in_transit inventory to each day (i+1) through (i + LT - 1)
-                demand_data.loc[i + 1: i + lead_time, 'in_transit_il'] = demand_data.loc[i + 1: i + lead_time,
-                                                                         'in_transit_il'] + reorder_qty
+                    # add in_transit inventory to each day (i+1) through (i + LT - 1)
+                    demand_data.loc[i + 1: i + lead_time, 'in_transit_il'] = demand_data.loc[i + 1: i + lead_time,
+                                                                             'in_transit_il'] + reorder_qty
 
-    demand_data['unmet_demand'] = (demand_data['new_daily_demand'] - demand_data['on_hand_il']).clip(lower=0)
-    results = calc_metrics(demand_data, reorder_qty)
-    return results, demand_data
+        demand_data['unmet_demand'] = (demand_data['new_daily_demand'] - demand_data['on_hand_il']).clip(lower=0)
+        results = calc_metrics(demand_data, reorder_qty)
+        return results, demand_data
 
 # run the sim
 test_results2 = sim_il(demand_data, rq_inputs)
 # unpack tuple
-results2, demand_data2  = test_results2
+results2, demand_data2 = test_results2
 
 # save data
 demand_data2.to_csv('demand_data_sim1.csv')
