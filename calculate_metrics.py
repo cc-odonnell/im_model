1a21dfb34fe2fcfecb4e4ce40d76263eb42492fe
Cecilia Odonnell <cici@Cecilias-MacBook-Pro.local>
2024-10-03 13:16:40 -0700

    add more metrics

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -8,34 +8,39 @@ def calc_metrics(demand_data, reorder_qty):
     grouped_df = demand_data[demand_data['order_date'] > '2023-01-31']
 
     # calculate metrics
+    # grouping by dc and item_num to preserve sku-loc names in resulting df
     grouped_df = grouped_df.groupby(['dc', 'item_num']).agg({
         'delivery': 'sum',
         'unmet_demand': ['sum', lambda x: (x > 0).sum()],
         'on_hand_il': ['mean', 'max', 'min'],
-        'new_daily_demand': 'mean'
+        'new_daily_demand': ['sum', 'mean'],
+        'order_date': 'count'
     })
 
     # rename the columns
-    grouped_df.columns = ['total_units_delivered', \
-                          'fill_rate_pct_met_demand_qty', \
-                          'fill_rate_pct_met_demand_days', \
+    grouped_df.columns = ['units_delivered_total', \
+                          'unmet_demand_qty', \
+                          'unmet_demand_days', \
                           'daily_il_mean', 'daily_il_max', 'daily_il_min', \
-                          'daily_demand_mean'
+                          'demand_total', 'daily_demand_mean', \
+                          'days_of_sim'
                           ]
 
     # reset index
     grouped_df = grouped_df.reset_index()
 
     # calculate subsequent variables
-    grouped_df['total_deliveries'] = grouped_df['total_units_delivered'] / reorder_qty
+    grouped_df['deliveries_total'] = grouped_df['units_delivered_total'] / reorder_qty
     grouped_df['days_of_supply_mean'] = grouped_df['daily_il_mean']/grouped_df['daily_demand_mean']
     grouped_df['days_of_supply_max'] = grouped_df['daily_il_max']/grouped_df['daily_demand_mean']
     grouped_df['days_of_supply_min'] = grouped_df['daily_il_min']/grouped_df['daily_demand_mean']
+    grouped_df['fill_rate_days'] = grouped_df['unmet_demand_days']/grouped_df['days_of_sim']
+    grouped_df['fill_rate_qty'] = grouped_df['unmet_demand_qty']/grouped_df['demand_total']
 
     return grouped_df
 
 
-'''# test function
+# test function
 sim_demand_data = pd.read_csv('demand_data_sim1.csv')
 reorder_qty = rq_inputs.loc[rq_inputs.index[0], 'reorder_qty']
 test_results = calc_metrics(sim_demand_data, reorder_qty)
