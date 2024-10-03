27e65874907a4319b8e4494a54124086fe0afb96
Cecilia Odonnell <cici@Cecilias-MacBook-Pro.local>
2024-10-03 10:23:57 -0700

    convert to function

--- single_year_views.py
+++ single_year_views.py
@@ -5,21 +5,17 @@ import matplotlib.pyplot as plt
 # data saved from simulation
 demand_data2.to_csv('demand_data_sim1.csv')
 
+def year_plot(demand_data):
+    plt.figure(figsize=(10, 6))
 
-#########################
+    # Plot 'on_hand_il' with red color
+    plt.plot(demand_data['order_date'], demand_data['on_hand_il'], color='red', label='on_hand_il')
 
-# generate diagnostic plots
+    # Plot 'new_daily_demand' with blue color
+    plt.plot(demand_data['order_date'], demand_data['new_daily_demand'], color='blue', label='new_daily_demand')
 
-plt.figure(figsize=(10, 6))
+    plt.title('R = 10, Q = 5')
+    plt.legend()
+    plt.show()
 
-# Plot 'on_hand_il' with red color
-plt.plot(demand_data['order_date'], demand_data['on_hand_il'], color='red', label='on_hand_il')
-
-# Plot 'new_daily_demand' with blue color
-plt.plot(demand_data['order_date'], demand_data['new_daily_demand'], color='blue', label='new_daily_demand')
-
-plt.title('R = 10, Q = 5')
-plt.legend()
-plt.show()
-
-####################
+year_plot(data_results)
